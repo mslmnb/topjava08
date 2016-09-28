335427c690e4079a25af569280677bd855965fdd
mslmnb <mslmn@rambler.ru>
2016-09-28 13:34:38 +0600

    отдельный запрос для очистики счетчика БД

---
+++ src/main/java/ru/javawebinar/topjava/util/exception/DbPopClearSequence.java
@@ -0,0 +1,27 @@
+package ru.javawebinar.topjava.util.exception;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by Mussulmanbekova_GE on 28.09.2016.
+ */
+public class DbPopClearSequence extends ResourceDatabasePopulator {
+    private static final ResourceLoader RESOURCE_LOADER = new DefaultResourceLoader();
+
+    @Autowired
+    private DataSource dataSource;
+
+    public DbPopClearSequence(String scriptLocation) {
+        super(RESOURCE_LOADER.getResource(scriptLocation));
+    }
+
+    public void execute() {
+        DatabasePopulatorUtils.execute(this, dataSource);
+    }
+}
