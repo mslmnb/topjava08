2776185105434bb76252db82e872d8f33786fcbe
mslmnb <mslmn@rambler.ru>
2016-10-06 14:54:48 +0600

    отдельный запрос для очистики счетчика БД

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,7 +1,10 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -10,11 +13,36 @@ import java.time.LocalTime;
  * GKislin
  * 11.01.2015.
  */
+
+    @NamedQueries({
+        @NamedQuery(name=Meal.DELETE, query="DELETE FROM Meal m WHERE m.id = :id"),
+        @NamedQuery(name=Meal.BY_USER_ID, query = "SELECT m FROM Meal m  WHERE m.id = : id AND m.user.id = : userId"),
+        @NamedQuery(name=Meal.FILTERED_AND_SORTED, query = "SELECT m FROM Meal m " +
+                            "WHERE m.user.id = : userId AND m.dateTime >= :startDateTime AND m.dateTime<:endDateTime " +
+                            "ORDER BY m.dateTime DESC" ),
+        @NamedQuery(name=Meal.ALL_SORTED, query="SELECT m FROM Meal m WHERE m.user.id = :userId ORDER BY m.dateTime"),
+    }
+    )
+@Entity
+@Table(name = "Meals", uniqueConstraints = {@UniqueConstraint(columnNames={"user_id","date_time"}, name="meals_unique_user_datetime_idx")})
 public class Meal extends BaseEntity {
+
+    public final static String FILTERED_AND_SORTED = "Meal.getFiltered&Sorted";
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String DELETE = "Meal.delete";
+    public static final String BY_USER_ID ="Meal.getByUserId";
+
+
+    @Column(name="date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name="description", nullable = false)
+    @NotEmpty
     private String description;
 
+    @Column(name="calories", nullable = false)
+    @Digits(integer = 4, fraction = 0)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
