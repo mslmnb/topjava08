190a2617acd0cefe587262a31515757fe2638015
mslmnb <mslmn@rambler.ru>
2016-10-15 16:54:48 +0600

    lesson 05

--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
@@ -2,10 +2,11 @@ package ru.javawebinar.topjava.service.jpa;
 
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 /**
  * Created by Gala on 15.10.2016.
  */
 @ActiveProfiles(Profiles.JPA)
-public class JpaUserServiceTest {
+public class JpaUserServiceTest extends AbstractUserServiceTest {
 }
