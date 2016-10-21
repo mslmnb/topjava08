5bed6fe45644c69407c3750e4a575235c10f1b0c
mslmnb <mslmn@rambler.ru>
2016-10-21 11:59:17 +0600

    отдельный запрос для очистики счетчика БД

--- src - копия/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++
@@ -1,9 +0,0 @@
-package ru.javawebinar.topjava.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-abstract public class AbstractMealServiceTest {
-    @Autowired
-    protected MealService service;
-
-}
